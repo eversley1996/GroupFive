10077153ce5a4d9afe5d74750d63e103a5ad4a49
eversley1996 <everboy15@hotmail.com>
2019-11-21 23:55:06 -0500

    Added Comments

--- src/mgmt/STAPM.java
+++ src/mgmt/STAPM.java
@@ -1,37 +1,49 @@
 package mgmt;
 
+import java.io;
 import java.util.ArrayList;
 
-public class STAPM {
+
+public final class STAPM {
 	private ArrayList<Item> items = new ArrayList<Item>();
-//	items.add("");
-//	items.add("");
-//	items.add("");
+	private String name = "Steer Town Auto Parts";
+	private User currentUser; // We will use this for access control
+
+	/* E.g, if (currentUser.getUserType == User.OWNER){
+				write code to get financial report
+			else{
+				tell the user they dont haver permission
+			}
+	}
+	*/
 	
 	public STAPM() {
 		
 	}
 	
-	public static void main(String args[]) {
-		STAPM s = new STAPM();
-	}
-	
 	public void addStock(String name, int quantity, double price,String manufacturer, 
-			boolean taxable, String date)
-	{
-		Item e = new Item(name,quantity,price,manufacturer, taxable);
-		e.setDate(date);
-		this.items.add(e);
+			boolean taxable, String date){
+		Item stockItem = new Item(name,quantity,price,manufacturer, taxable);
+		stockItem.setDate(date);
+		this.items.add(stockItem);
 	}
 	
-	public Item search(String itemName,ArrayList<Item> items )
+	/* 
+	Here I think we accept a string, look throught the arraylist and print the toString() for the Item that matches,
+	then return a string instead of an Item object
+	*/
+	public Item search(String itemName)
 	{
 		for(Item i:items) {
 			if(i.getItemName().equalsIgnoreCase(itemName)) {
-				return i;
+				return i; // return i.toString() instead
 			}
 		}
-		return null;
+		return null; // return "Item not found" instead
+	}
+
+	public void addCurrentUSer(User user){
+		this.currentUser = user;
 	}
 }
 
